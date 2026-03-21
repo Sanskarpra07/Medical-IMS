a0cb9af5b7f26cf9d4cb2fe0d9ea994b91d2e444
Sanskarpra07 <pradhansanskar07@gmail.com>
2026-03-21 19:06:35 +0545

    [FIX] Correct error handling in database connection to use mysqli_connect_error()

--- db.php
+++ db.php
@@ -7,6 +7,6 @@ $dbName="db_dummy";
 //Creating Connection
 $conn=mysqli_connect($servername,$username,$password,$dbName);
 if (!$conn) {
-    die("Connection Failed:" . mysqli_error());
+    die("Connection Failed:" . mysqli_connect_error());
 }
 ?>
